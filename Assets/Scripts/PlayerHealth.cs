65fe003bd349678f8a524622817f932104180739
Filip Gocławski <s24471@pjwstk.edu.pl>
2023-10-11 23:04:25 +0200

    health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int health;
+    
+    private Animator animator;
+
+    private void Start()
+    {
+        health = maxHealth;
+        animator = GetComponent<Animator>();
+    }
+
+    public void takeDamage(int dmg)
+    {
+        health -= dmg;
+        if(health <= 0)
+        {
+            animator.SetTrigger("Player_DeadHit");
+            Destroy(this.gameObject);
+            //@todo player death
+        }
+        else
+            animator.SetTrigger("Player_Hit");
+    }
+    public void heal(int heal)
+    {
+        health += heal;
+    }
+}
