972a138e7213c5b7b966d8c071c9e7e83a09aafe
KingaMendyk <s25275@pjwstk.edu.pl>
2023-10-18 17:10:05 +0200

    Camera
    Kamera zatrzymuje się w miejscu po smierci gracza

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    private PlayerHealth playerHealth;
+    private Vector3 lastCameraPos;
+
+    void Awake()
+    {
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if(player != null)
-            transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if (player != null)
+        {
+            if (playerHealth.health > 0)
+            {
+                transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+                lastCameraPos = transform.position;
+            }
+
+            else
+            {
+                transform.position = lastCameraPos;
+            }
+        }
     }
 }
