f53362b10037e807026371984ca0cd5d190f51bd
KingaMendyk <s25275@pjwstk.edu.pl>
2023-10-25 15:57:44 +0200

    Destructible Box
    [+] Podstawowy obiekt
    [+] Animacje dla pudła

---
+++ Assets/DestructibleBox.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleBox : MonoBehaviour
+{
+    private Animator _animator;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+    
+}
