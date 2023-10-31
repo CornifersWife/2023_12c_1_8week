f6f1a7423a412d0c424c299e342958cedff4a956
KingaMendyk <s25275@pjwstk.edu.pl>
2023-10-31 11:31:44 +0100

    Ulepszenie beczki
    Teraz beczka może zawierać więcej pieniążków na raz

--- Assets/Scripts/Destrucibles/DestructibleBarrel.cs
+++ Assets/Scripts/Destrucibles/DestructibleBarrel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,17 @@ using UnityEngine;
 public class DestructibleBarrel : DestructibleBox
 {
     [SerializeField] private GameObject containObject;
+    [SerializeField] private int objectCount = 1;
 
     public override void SwitchToShattered()
     {
         Instantiate(shatteredVersion, transform.position, transform.rotation);
-        Instantiate(containObject, transform.position, transform.rotation);
+        for (int i = 0; i < objectCount; i++)
+        {
+            float offset = i * 0.1f;
+            Vector3 pos = transform.position + transform.right * offset;
+            Instantiate(containObject, pos, transform.rotation);
+        }
         Destroy(gameObject);
     }
 }
