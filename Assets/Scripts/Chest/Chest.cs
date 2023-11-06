ebc9782a27f605a2986f58df78d9e24ebde071f7
Filip Gocławski <s24471@pjwstk.edu.pl>
2023-11-06 17:18:33 +0100

    smaller collider after opening

--- Assets/Scripts/Chest/Chest.cs
+++ Assets/Scripts/Chest/Chest.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
+
+    [SerializeField] private Collider2D closedCollisionBox; 
+    [SerializeField] private Collider2D openedCollisionBox;  
+
     [SerializeField] private GameObject treasure;
     [SerializeField] private float summonDelay = 1f;
     [SerializeField] private string keyName;
+
     Animator animator;
     bool isOpened;
     void Awake()
     {
         animator = GetComponent<Animator>();
         isOpened = false;
+        openedCollisionBox.enabled = false;
+        closedCollisionBox.enabled = true;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -28,5 +35,7 @@ public class Chest : MonoBehaviour
     {
         yield return new WaitForSeconds(summonDelay);
         Instantiate(treasure, new Vector2(transform.position.x, transform.position.y + 0.30f), transform.rotation);
+        openedCollisionBox.enabled = true;
+        closedCollisionBox.enabled = false;
     }
 }
